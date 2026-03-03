d159f5bd2817101481f01691c34014dc91524193
tarun.vaya <tarun.vaya@gmail.com>
2026-03-03 14:01:02 +0530

    App Identity in Project

--- MTenantSolution/MTenantSolution.API/Program.cs
+++ MTenantSolution/MTenantSolution.API/Program.cs
@@ -1,7 +1,17 @@
 using Microsoft.OpenApi.Models;
 using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MTenantSolution.API.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+
 
 // Add services to the container.
 
