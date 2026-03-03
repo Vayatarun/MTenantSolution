bddf2f14bfa2867ffd42d1240d2879653094df53
tarun.vaya <tarun.vaya@gmail.com>
2026-03-03 11:29:34 +0530

    Create Public Controller

---
+++ MTenantSolution/MTenantSolution.API/Areas/PublicArea/PublicController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace MTenantSolution.API.Areas.PublicArea
+{
+    [Area("PublicArea")]
+    [DisplayName("Public Controller")]
+    [Route("api/[area/controller]")]
+    [ApiController]
+    public class PublicController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is Public Area");
+        }
+
+    }
+}
