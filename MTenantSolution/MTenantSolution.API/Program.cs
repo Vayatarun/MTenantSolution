5e4d20e9ca2dad7936af8a083727c325292fa9e0
tarun.vaya <tarun.vaya@gmail.com>
2026-03-03 11:37:41 +0530

    add Swagger in Project

--- MTenantSolution/MTenantSolution.API/Program.cs
+++ MTenantSolution/MTenantSolution.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,13 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c=>
+{     c.SwaggerDoc("v1", new OpenApiInfo
+{
+    Version = "v1",
+    Title = "MTenantSolution API",
+});
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MTenantSolution API V1");
+    });
 }
 
 app.UseHttpsRedirection();
